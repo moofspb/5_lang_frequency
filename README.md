44ccd34c62446272573ba34a91eef0e28eb01eb4
moofspb <moofspb@gmail.com>
2017-02-21 12:30:03 +0000

    Write two functions, module works

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The python module takes a path to a text file and prints a list of 10 most frequent words in this text.
+
+# Usage
+
+`python lang_frequency.py <path to a file>`
+
+For example:
+
+```#!bash
+
+$ python lang_frequency.py war_and_peace.txt
+
+There are 10 most frequent words in the text:
+'the': 31954 times
+'and': 21214 times
+'to': 16514 times
+'of': 14939 times
+'a': 10129 times
+'in': 8499 times
+'he': 8140 times
+'that': 7817 times
+'his': 7677 times
+'was': 7343 times
+
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,25 @@
+import os
+import re
+import sys
+import collections
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as handler:
+        return handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r'[a-zA-Zа-яА-Я]+', text)
+    counts = collections.Counter(words)
+    most_common_words = counts.most_common(10)
+    return most_common_words
 
 
 if __name__ == '__main__':
-    pass
+    most_frequent_words = get_most_frequent_words(load_data(sys.argv[1]))
+    print('There are 10 most frequent words in the text:')
+    for word, count in most_frequent_words:
+        print("'{}': {} times".format(word, count))
